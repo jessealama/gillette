068a6e1468b78671eeb9f4a6188851056cb41233
jesse <jesse@serverracket.com>
2021-07-19 06:15:01 +0000

    Add missing info.rkt
    FossilOrigin-Name: 80bb8d75bb64b0185192d2e509f0edee58861fb449cef9ec8c23f74d6be09ed9

---
+++ gillette-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define version "0.2")
+(define collection "gillette")
+(define description "Implementation for Gillette")
+(define authors '("jesse@serverracket.com"))
+(define deps '("base"
+               "typed-racket-lib"
+               "txexpr"
+               "http-easy"))
+(define build-deps '("rackunit-lib"
+                     "rackunit-typed"))
